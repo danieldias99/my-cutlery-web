152eca795ad52ad6622b2048d5da3f76a5c090b4
Gonçalo <goncalo7bfc@gmail.com>
2019-10-24 17:55:44 +0100

    [Implementation] MaquinaController created and MaquinaRepositorio done

---
+++ LAPR5_3DD_019/Models/Repositorios/MaquinaRepositorio.cs
@@ -0,0 +1,27 @@
+using LAPR5_3DD_019.Models.ClassesDeDominio;
+using LAPR5_3DD_019.Models.DTO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using LAPR5_3DD_019.Models.ValueObjects;
+
+namespace LAPR5_3DD_019.Models.Repositorios{
+
+    public class MaquinaRepositorio{
+
+        private readonly LAPR5DBContext _context;
+
+        public MaquinaRepositorio(LAPR5DBContext context){
+            _context = context;
+        }
+
+        public async Task<ActionResult<MaquinaDTO>> getMaquinaByName(NomeMaquina nomeMaquina){
+            var maquina = await _context.Maquinas.FindAsync(nomeMaquina.nomeMaquina);
+            return maquina.toDTO();
+        }
+
+        public void addMaquina(Maquina newMaquina){
+            _context.Maquinas.Add(newMaquina);
+            _context.SaveChanges();
+        }
+    }
+}

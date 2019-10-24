8e288c9e74c233eb70f869fcd01bb4509da27b88
Gonçalo <goncalo7bfc@gmail.com>
2019-10-24 17:56:42 +0100

    [Implementation] Maquina Controller created and MaqRepositorio done #6

---
+++ LAPR5_3DD_019/Controllers/MaquinaController.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using LAPR5_3DD_019.Models.ClassesDeDominio;
+using LAPR5_3DD_019.Models;
+using LAPR5_3DD_019.Models.DTO;
+using LAPR5_3DD_019.Models.Repositorios;
+
+namespace LAPR5_3DD_019.Controllers {
+
+  [Route("api/Maquina")]
+  [ApiController]
+  public class MaquinaController : ControllerBase {}
+}

797ec3fb9aaa7d768f92f5837dd4053ed35d7dfb
1170790 <1170790@isep.ipp.pt>
2019-10-27 15:14:38 +0000

    Atualizar/ Mudar Operação, com recurso a PUT Request [Implementação]

--- LAPR5_3DD_019/Models/ValueObjects/DuracaoOperacao.cs
+++ LAPR5_3DD_019/Models/ValueObjects/DuracaoOperacao.cs
@@ -4,16 +4,16 @@ namespace LAPR5_3DD_019.Models.ValueObjects
 {
     public class DuracaoOperacao : ValueObject
     {
-        public long Id { get; set; }
         public int hora { get; set; }
         public int min { get; set; }
         public int seg { get; set; }
 
-        public DuracaoOperacao(){}
+        public DuracaoOperacao() { }
 
-        public DuracaoOperacao(int hora, int min, int sec){this.hora=hora; this.min=min; this.seg=seg;}
+        public DuracaoOperacao(int hora, int min, int sec) { this.hora = hora; this.min = min; this.seg = seg; }
 
-        public string toString(){
+        public string toString()
+        {
             return hora.ToString() + ":" + min.ToString() + ":" + seg.ToString();
         }
     }

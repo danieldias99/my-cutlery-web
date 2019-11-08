7583cb9089facfda7637f32f0320d31864b04bad
1170790 <1170790@isep.ipp.pt>
2019-11-08 10:39:12 +0000

    Inicialização da implementação do controlo da camara para visualizar o chão de fábrica [refs #28]

--- Solution/VE/ThreeJS.js
+++ Solution/VE/ThreeJS.js
@@ -1,24 +1,37 @@
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+var scene;
+
+var player = { height: 1.8 };
+scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera(100, window.innerWidth / window.innerHeight, 0.1, 1000);
 
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-var geometry = new THREE.BoxGeometry(1, 1, 1);
-var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-var cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+var controls = new THREE.OrbitControls(camera, renderer.domElement);
 
-camera.position.z = 5;
+var floor = function () {
+    var plane_geometry = new THREE.PlaneGeometry(60, 25);
+    var material = new THREE.MeshBasicMaterial({ color: 0x858585, side: THREE.DoubleSide });
+    plane_geometry.rotateX(15);
+    var plane = new THREE.Mesh(plane_geometry, material);
+    scene.add(plane);
+}
+
+floor();
+
+var set_camera_init = function () {
+    camera.position.z = 25;
+}
+
+set_camera_init();
 
 var animate = function () {
     requestAnimationFrame(animate);
 
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
+    //cube.rotation.x += 0.01;
+    //cube.rotation.y += 0.01;
 
     renderer.render(scene, camera);
 };
-
 animate();

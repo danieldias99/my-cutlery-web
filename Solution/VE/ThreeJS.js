2b288abc343671e6644ae8d59af1f8509ccee001
AMata99 <1170785@isep.ipp.pt>
2019-11-14 10:29:12 +0000

    Criação do eliminar máquina [Fixes #31]

--- Solution/VE/ThreeJS.js
+++ Solution/VE/ThreeJS.js
@@ -1,4 +1,5 @@
 var linhaTemp = [];
+var maquinasTemp = [];
 
 var buttons = document.getElementsByTagName("button");
 for (let i = 0; i < buttons.length; i++) {
@@ -36,8 +37,8 @@ function onButtonClickM(event) {
 
 //botao eliminar maquina
 function onButtonClickApagarM(event) {
-    if (contMaquinas > 0) {
-        //apagarMaquina();
+    if (contMaquinasTotal > 0) {
+        apagarMaquina();
     } else {
         alert("Não existe nenhuma máquina!");
     }
@@ -172,6 +173,7 @@ var apagarLinha = function () {
 //Desenhar maquina - widget
 var contTapetesPreenchidos = 0;
 var contMaquinas = 0;
+var contMaquinasTotal = 0;
 
 var TAMANHO_MAQUINA = 3;
 var SIDEWALLK = 5;
@@ -181,8 +183,9 @@ var desenhaMaquina = function (size_m, x, y, z) {
     var geometry_balcao = new THREE.BoxGeometry(size_m, size_m, size_m);
     var material = new THREE.MeshBasicMaterial({ color: 0x000000 });
     geometry_balcao.translate(x, y, z);
-    var linha = new THREE.Mesh(geometry_balcao, material);
-    scene.add(linha);
+    var maquina = new THREE.Mesh(geometry_balcao, material);
+    scene.add(maquina);
+    maquinasTemp.push(maquina);
 }
 
 var desenhaMaquinas = function () {
@@ -190,15 +193,31 @@ var desenhaMaquinas = function () {
         case 0:
             desenhaMaquina(TAMANHO_MAQUINA, - LARGURA_FABRICA / 2 + (SIDEWALLK + MACHINE_SPACE + (contMaquinas * (TAMANHO_MAQUINA + MACHINE_SPACE))), -17, - LARGURA_FABRICA / 2 + (8 + (2 * contTapetesPreenchidos * LARGURA_TAPETE) - TAMANHO_MAQUINA / 2)); //primeiras maquinas
             contMaquinas++;
+            contMaquinasTotal++;
             break;
         case 1:
             desenhaMaquina(TAMANHO_MAQUINA, - LARGURA_FABRICA / 2 + (SIDEWALLK + MACHINE_SPACE + (contMaquinas * (TAMANHO_MAQUINA + MACHINE_SPACE))), -17, - LARGURA_FABRICA / 2 + (8 + (2 * contTapetesPreenchidos * LARGURA_TAPETE) - TAMANHO_MAQUINA / 2)); //segundas maquinas
             contMaquinas++;
+            contMaquinasTotal++;
             break;
         default:
             desenhaMaquina(TAMANHO_MAQUINA, - LARGURA_FABRICA / 2 + (SIDEWALLK + MACHINE_SPACE + (contMaquinas * (TAMANHO_MAQUINA + MACHINE_SPACE))), -17, - LARGURA_FABRICA / 2 + (8 + (2 * contTapetesPreenchidos * LARGURA_TAPETE) - TAMANHO_MAQUINA / 2));// terceiras maquinas
             contTapetesPreenchidos++;
             contMaquinas = 0;
+            contMaquinasTotal++;
+    }
+}
+
+//Apagar uma maquina - widget
+var apagarMaquina = function () {
+    var mqn = maquinasTemp.pop();
+    scene.remove(mqn);
+    contMaquinasTotal--;
+    if(contMaquinas == 0){
+        contMaquinas = 2;
+        contTapetesPreenchidos--;
+    }else{
+        contMaquinas--;
     }
 }
 

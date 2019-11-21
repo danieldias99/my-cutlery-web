100da17b0f0ff712bb3ad4e1cde344a2d0d55e6b
1170790 <1170790@isep.ipp.pt>
2019-11-21 15:34:53 +0000

    Changes on serve.js to run app in Heroku.

--- FrontEnd/MyOwnCutlery/server.js
+++ FrontEnd/MyOwnCutlery/server.js
@@ -2,11 +2,11 @@ const express = require('express');
 const path = require('path');
 const nomeApp = process.env.npm_package_name;
 const app = express();
- 
-app.use(express.static(`$/dist/$`));
- 
+
+app.use(express.static('./dist/MyOwnCutlery'));
+
 app.get('/*', (req, res) => {
-res.sendFile(path.join(`$/dist/$/index.html`));
+    res.sendFile(path.join('./dist/MyOwnCutlery/index.html'));
 });
- 
+
 app.listen(process.env.PORT || 8080);

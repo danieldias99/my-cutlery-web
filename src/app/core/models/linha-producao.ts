6492a30561c49503f68655d64fb16de2337db515
1170790 <1170790@isep.ipp.pt>
2019-12-01 00:35:45 +0000

    Implementação da definição das ferramentas de desenho para a visualização [refs #72]

--- src/app/core/models/linha-producao.ts
+++ src/app/core/models/linha-producao.ts
@@ -2,10 +2,21 @@ import { Maquina } from './maquina.model';
 
 export class LinhaProducao {
     id: string;
+    descricao: string;
+    posicao_x: string;
+    posicao_y: string;
+    orientacao: string;
+    comprimento: string;
+    largura: string;
     maquinas: Maquina[];
 
-    constructor(id: string, maquinas: Maquina[]) {
+    constructor(id: string, descricao: string, posicao_x: string, posicao_y: string, orientacao: string, comprimento: string, largura: string) {
         this.id = id;
-        this.maquinas = maquinas;
+        this.descricao = descricao;
+        this.orientacao = orientacao;
+        this.posicao_x = posicao_x;
+        this.posicao_y = posicao_y;
+        this.comprimento = comprimento;
+        this.largura = largura;
     }
 }
--- src/app/layout/content/linha-producao/linha-producao.component.ts
+++ src/app/layout/content/linha-producao/linha-producao.component.ts
@@ -3,8 +3,6 @@ import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 
 import { LinhaProducaoService } from 'src/app/core/services/linha-producao/linha-producao.service';
-import { MaquinaService } from 'src/app/core/services/maquina/maquina.service';
-import { Maquina } from 'src/app/core/models/maquina.model';
 import { LinhaProducao } from 'src/app/core/models/linha-producao';
 
 @Component({
@@ -14,75 +12,50 @@ import { LinhaProducao } from 'src/app/core/models/linha-producao';
 })
 export class LinhaProducaoComponent implements OnInit {
 
-  maquinasAssociadas: Maquina[];
-  allMaquinas: Maquina[];
   allLinhasProducao: LinhaProducao[];
   statusMessage: string;
 
   constructor(private linhaProducaoSrv: LinhaProducaoService,
-    private maquinaSrv: MaquinaService,
     private route: ActivatedRoute,
     private location: Location) { }
 
-    ngOnInit() {
-      this.getTiposMaquina();
-      this.getOperacoesDisponiveis();
-      this.maquinasAssociadas = new Array;
-    }
-  
-    private getTiposMaquina(): void {
-      this.linhaProducaoSrv.getLinhasProducao().subscribe(
-        data => { console.log(data); this.allLinhasProducao = data; },
-        error => { this.statusMessage = "Error: Service Unavailable" });
-    }
-  
-    private getOperacoesDisponiveis() {
-      this.maquinaSrv.getMaquinas().subscribe(data => { console.log(data); this.allMaquinas = data },
-        error => { this.statusMessage = "Error: Service Unavailable" })
-    }
-  
-    assMaquinaLinhaProducao(Id: number) {
-      var maquina = this.allMaquinas.find(maquina => maquina.id === Id);
-      this.allMaquinas = this.allMaquinas.filter(h => h.id !== Id);
-      this.maquinasAssociadas.push(maquina);
-    }
-  
-    deleteAssMaquinaLinhaProducao(Id: number){
-      var maquina = this.maquinasAssociadas.find(maquina => maquina.id === Id);
-      this.maquinasAssociadas = this.maquinasAssociadas.filter(h => h.id !== Id);
-      this.allMaquinas.push(maquina);
-    }
-  
-    /**
-     * Add LinhaProducao to API MDF
-     * @param IdLinhaProducao Id of an operation
-     * @param DescricaoLinhaProducao brief description what the operation is about
-     */
-    addLinhaProducao(IdLinhaProducao: string): void {
-      if (!IdLinhaProducao) {
-        return;
-      }
-  
-      this.linhaProducaoSrv.addLinhaProducao(new LinhaProducao(IdLinhaProducao, this.maquinasAssociadas))
-        .subscribe(
-          LinhaProducao => { this.allLinhasProducao.push(LinhaProducao); },
-          error => { this.statusMessage = "Error: Service Unavailable"; }
-        );
-      this.maquinasAssociadas = new Array;
-      this.getOperacoesDisponiveis();
-    }
-  
-    delete(LinhaProducao: LinhaProducao): void {
-      this.allLinhasProducao = this.allLinhasProducao.filter(h => h !== LinhaProducao);
-      this.linhaProducaoSrv.deleteLinhaProducao(LinhaProducao.id).subscribe(res => {
-        console.log(res);
-      }, error => {
-        console.log(error);
-      });
-    }
-  
-    goBack(): void {
-      this.location.back();
+  ngOnInit() {
+    this.getLinhasProducao();
+  }
+
+  private getLinhasProducao(): void {
+    this.linhaProducaoSrv.getLinhasProducao().subscribe(
+      data => { console.log(data); this.allLinhasProducao = data; },
+      error => { this.statusMessage = "Error: Service Unavailable" });
+  }
+
+  /**
+   * Add LinhaProducao to API MDF
+   * @param IdLinhaProducao Id of an operation
+   * @param DescricaoLinhaProducao brief description what the operation is about
+   */
+  addLinhaProducao(IdLinhaProducao: string, descricao: string, posicao_x: string, posicao_y: string, orientacao: string, comprimento: string, largura: string): void {
+    if (!IdLinhaProducao) {
+      return;
     }
 
+    this.linhaProducaoSrv.addLinhaProducao(new LinhaProducao(IdLinhaProducao, descricao, posicao_x, posicao_y, orientacao, comprimento, largura))
+      .subscribe(
+        LinhaProducao => { this.allLinhasProducao.push(LinhaProducao); },
+        error => { this.statusMessage = "Error: Service Unavailable"; }
+      );
+  }
+
+  delete(LinhaProducao: LinhaProducao): void {
+    this.allLinhasProducao = this.allLinhasProducao.filter(h => h !== LinhaProducao);
+    this.linhaProducaoSrv.deleteLinhaProducao(LinhaProducao.id).subscribe(res => {
+      console.log(res);
+    }, error => {
+      console.log(error);
+    });
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
 }

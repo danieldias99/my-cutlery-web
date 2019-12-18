1d7c0588b12a5337c8775617a8a52ef7a18df915
1170785 <1170785@isep.ipp.pt>
2019-12-18 01:16:40 +0000

    Implementação do cancelar uma encomenda [Implementação][refs #95]

--- src/app/layout/content/sessao/sessao.component.ts
+++ src/app/layout/content/sessao/sessao.component.ts
@@ -1,9 +1,11 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, OnInit, Input, NgModule } from '@angular/core';
+import { Router } from '@angular/router';
 
-import { Utilizador } from 'src/app/core/models/utilizador';
 import { UtilizadorService } from 'src/app/core/services/utilizador/utilizador.service';
 import { Cliente } from 'src/app/core/models/cliente';
+import { EncomendaService } from 'src/app/core/services/encomenda/encomenda.service';
+import { Encomenda } from 'src/app/core/models/encomenda';
+
 
 @Component({
   selector: 'app-sessao',
@@ -14,21 +16,79 @@ export class SessaoComponent implements OnInit {
 
   @Input() currentUser: Cliente;
 
+  @Input() admin: Boolean;
+
+  encomendas: Encomenda[];
+
+  encomendasFilter: Encomenda[];
+
   constructor(private utilizadorSrv: UtilizadorService,
-    private route: ActivatedRoute) { }
+    private encomendaSrv: EncomendaService,
+    private router: Router) { }
 
   ngOnInit() {
     this.getCurrentUser();
   }
 
   getCurrentUser() {
-    const nr_idCivil = +this.route.snapshot.paramMap.get('id');
-    this.utilizadorSrv.getUser(nr_idCivil)
+    this.utilizadorSrv.getUser()
       .subscribe(utilizadorResult => {
-        this.currentUser = utilizadorResult[0];
-        console.log(utilizadorResult)
+        this.currentUser = utilizadorResult;
+        if (this.currentUser.isAdmin) {
+          this.admin = true;
+          this.getEncomendas();
+        } else {
+          this.getEncomendasByCliente();
+        }
+        console.log(utilizadorResult);
       },
         error => "Update Service Unavailable");
+  }
+
+  getEncomendasByCliente() {
+    this.encomendaSrv.getEncomendasByUser()
+      .subscribe(encomendasResult => {
+        this.encomendas = encomendasResult;
+        console.log(encomendasResult);
+      },
+        error => "Update Service Unavailable");
+  }
+
+  getEncomendas() {
+    this.encomendaSrv.getEncomendas()
+      .subscribe(encomendasResult => {
+        this.encomendas = encomendasResult;
+        this.encomendasFilter = encomendasResult;
+        console.log(encomendasResult);
+      },
+        error => "Update Service Unavailable");
+  }
+
+  newEncomenda() {
+    this.router.navigate(['/encomenda']);
+  }
+
+  cancelar(encomenda: Encomenda) {
+    if (this.currentUser.isAdmin) {
+      this.encomendaSrv.cancelar(encomenda._id).subscribe(_ => {
+        this.getEncomendas();
+      },
+        error => "Update Service Unavailable");
+    } else {
+      this.encomendaSrv.pedidoCancelar(encomenda._id).subscribe(_ => {
+        this.getEncomendasByCliente();
+      },
+        error => "Update Service Unavailable");
+    }
+  }
+
+  update(encomenda: Encomenda) {
+    this.router.navigate(['/encomenda-detail/' + encomenda._id]);
+  }
+
+  filtrarEncomendas(toFilter: string) {
+    this.encomendasFilter = this.encomendas;
+    this.encomendasFilter = this.encomendas.filter(c => c.cliente.toLowerCase().includes(toFilter));
   }
 
 }

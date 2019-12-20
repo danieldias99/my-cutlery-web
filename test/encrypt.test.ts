c035ca38b9944a2d16d9c2769b66d65c03be53eb
Gonçalo <goncalo7bfc@gmail.com>
2019-12-20 15:39:34 +0000

    [UnitTest] Added coverage to encryption and added coverage config file

--- test/encrypt.test.ts
+++ test/encrypt.test.ts
@@ -3,9 +3,9 @@ import { EncriptPackage } from '../src/app/core/models/EncriptPackage';
 
 let email: String = 'goncalo@goncalo';
 let password: String = 'pwtest';
+let encryptionTestOBJ = new EncriptPackage(email, password);
 
 it('encryptionTest', () => {
-    let encryptionTestOBJ = new EncriptPackage(email, password);
     expect(encryptionTestOBJ.email).to.eq('goncalo@goncalo');
     expect(encryptionTestOBJ.password).to.eq('pwtest');
 })

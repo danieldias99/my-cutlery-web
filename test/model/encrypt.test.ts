46382ddd6690cd87da11c2ee486da3e3143f2623
Gonçalo <goncalo7bfc@gmail.com>
2019-12-23 03:08:59 +0000

    [CodeSmells] Encrypt.Test name refactor

--- test/model/encrypt.test.ts
+++ test/model/encrypt.test.ts
@@ -1,11 +1,11 @@
 import { expect } from 'chai';
-import { EncriptPackage } from '../src/app/core/models/EncriptPackage';
+import { EncriptPackage } from '../../src/app/core/models/EncriptPackage';
 
 let email: String = 'goncalo@goncalo';
 let password: String = 'pwtest';
 let encryptionTestOBJ = new EncriptPackage(email, password);
 
-it('encryptionTest', () => {
+it('Encryption Test', () => {
     expect(encryptionTestOBJ.email).to.eq('goncalo@goncalo');
     expect(encryptionTestOBJ.password).to.eq('pwtest');
 })
